8f829989b70f707c4f7d8931de236c72d29d9939
rrjackso <robert.r.jackson@uscis.dhs.gov>
2018-05-06 13:26:17 +0000

    Fixing stuff

--- certs/put_cert_crossing.sh
+++ certs/put_cert_crossing.sh
@@ -1,11 +1,7 @@
- files=( svs-client-buildtest.crt  svs-client-preprod.crt   svs-server-buildtest.crt  svs-server-preprod.crt \
- svs-client-buildtest.pem  svs-client-preprod..pem  svs-server-buildtest.pem  svs-server-preprod.pem \
- svs-client-icamtest.crt   svs-client-preview.crt   svs-server-icamtest.crt   svs-server-preview.crt \
- svs-client-icamtest.pem   svs-client-preview.pem   svs-server-icamtest.pem   svs-server-preview.pem \
- svs-client-nonprod.crt    svs-client-prod.crt      svs-server-nonprod.crt    svs-server-prod.crt \
- svs-client-nonprod.pem    svs-client-prod.pem      svs-server-nonprod.pem    svs-server-prod.pem )
+ files=( client-build.crt  client-preprod.crt   server-build.crt  server-preprod.crt \
+   client-build.pem  client-preprod.pem  server-build.pem  server-preprod.pem )
 
  for filename in "${files[@]}"
  do
-   crossing put -b vis-certs -f ${filename} -k <key>
+   crossing put -b cert_folder -f ${filename} -k <key>
  done
