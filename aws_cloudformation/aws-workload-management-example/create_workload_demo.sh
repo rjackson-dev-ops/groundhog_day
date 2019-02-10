f2fb8b1d0a867626c7e61d23c5a33f0ce3a973ff
Robert Jackson <robert.jackson@stelligent.com>
2019-02-10 14:49:23 -0600

    Updating scripts

--- aws_cloudformation/aws-workload-management-example/create_workload_demo.sh
+++ aws_cloudformation/aws-workload-management-example/create_workload_demo.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -x
 aws cloudformation create-stack --stack-name RjacksonWorkloadDemo \
    --template-body file:////Users/rjackson/projects/groundhog_day/aws_cloudformation/aws-workload-management-example/workload-yaml-master.yml \
-  --parameters  file:///Users/rjackson/projects/groundhog_day/aws_cloudformation/aws-workload-management-example/workload_parameters.json 
+  --parameters  file:///Users/rjackson/projects/groundhog_day/aws_cloudformation/aws-workload-management-example/workload_parameters.json \
+  --capabilities CAPABILITY_IAM
--- devops/describe_stack_by_name.sh
+++ devops/describe_stack_by_name.sh
@@ -2,11 +2,12 @@
 
 # check parameters
 if [ $# -ne  1 ]; then
-   echo "Please enter get_stack_outputs.sh <stack_name>"
+   echo "Please enter describe_stack_by_name.sh <stack_name>"
    exit 1
 fi
 
 stack_name=$1
 
 echo "Stack Name values is $stack_name"
+set -x
 aws cloudformation describe-stacks --query "Stacks[?contains(StackName, '${stack_name}')]"
