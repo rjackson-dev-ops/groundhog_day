a0a83ff0f36164e653a9d65e16c5066940b07e10
Robert Jackson <robert.jackson@stelligent.com>
2019-02-23 09:12:43 -0600

    Adding placement for get instances script

--- devops/get_instances.sh
+++ devops/get_instances.sh
@@ -2,4 +2,5 @@
 
 set -x
 aws ec2 describe-instances --query "Reservations[].Instances[].{Name:Tags[?Key=='Name'] \
-| [0].Value,PublicIPAddress:Association.PublicIp,PrivateIpAddress:PrivateIpAddress,VpcId:VpcId,InstanceId:InstanceId,ImageId:ImageId,SubnetId:SubnetId,Status:State.Name}"
+| [0].Value,PublicIPAddress:Association.PublicIp,PrivateIpAddress:PrivateIpAddress,\
+AvailabilityZone:Placement.AvailabilityZone,VpcId:VpcId,InstanceId:InstanceId,ImageId:ImageId,SubnetId:SubnetId,Status:State.Name}"
